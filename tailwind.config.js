e00326ce11bbed88af671813a668fa88f6989524
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-28 01:56:22 -0300

    feat(styles): add custom colors to tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "ocean-blue": "#318ce7",
+                "sky-blue": "#4a9ae9",
+                "azure-blue": "#6aa9e9",
+                "light-sky": "#98c3ed",
+                "pale-blue": "#afcdea",
+            },
+        },
     },
     plugins: [],
 };
