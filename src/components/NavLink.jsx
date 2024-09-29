0ba3243e0ab5d7fb0a0935436bab3465b6e0b4ca
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-29 00:05:01 -0300

    style(NavLink): add hover effect

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NavLink({ children, path }) {
     return (
-        <Link to={path} className="text-[20px] text-sky-blue hover:underline">
+        <Link to={path} className="px-[10px] py-[4px] text-[20px] text-sky-blue hover:bg-pale-blue/[0.2] hover:rounded">
             {children}
         </Link>
     );
