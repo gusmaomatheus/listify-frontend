fd74c0ad005721aaabecc80a1a912f858d3676c5
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-28 22:59:24 -0300

    feat: create FeedbackCard component
    Display user testimonials on the home page.

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,20 @@
+import userLogo from "../assets/user.png";
+import Text from "./Text";
+
+export default function FeedbackCard({ name = "Customer", feedback = "Nice app!" }) {
+    return (
+        <div className="w-[200px] h-[200px] border-[1px] border-[#818589]/[0.3] rounded-[8px]">
+            <div className="h-[55%] flex flex-col justify-center items-center bg-pale-blue/[0.3] border-b-[1px] border-[#818589]/[0.3] space-y-[8px]">
+                <img src={userLogo} alt="User picture" className="w-[64px] h-[64px]" />
+                <Text as="p" size={14} color="#818589">
+                    {name}
+                </Text>
+            </div>
+            <div className="mt-[12px]">
+                <Text as="p" size={12} color="#818589" centralize>
+                    "{feedback}"
+                </Text>
+            </div>
+        </div>
+    );
+}
