35a885e82e697e92af1ae1ddf0720ae351a903d6
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-29 01:20:07 -0300

    style(NavLink): add animations
    Add animations in the component on the hover mouse event

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function NavLink({ children, path }) {
     return (
-        <Link to={path} className="px-[10px] py-[4px] text-[20px] text-sky-blue hover:bg-pale-blue/[0.2] hover:rounded">
+        <Link
+            to={path}
+            className="px-[10px] py-[4px] text-[20px] text-sky-blue transition duration-300 ease-in hover:bg-pale-blue/[0.3] hover:rounded"
+        >
             {children}
         </Link>
     );
