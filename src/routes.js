44f037ca2457a5734b9558026252afcd79a32615
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-30 18:20:03 -0300

    feat: create SignUp page
    Create a page responsible for registering new users and routing them
    through react-router.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import NotFound from "pages/NotFound";
 import About from "pages/About";
 import LandingPage from "pages/LandingPage";
+import SignUp from "pages/SignUp";
 
 export default function AppRoutes() {
     return (
@@ -12,6 +13,7 @@ export default function AppRoutes() {
                 <Route path="/" element={<LandingPage />}>
                     <Route path="/" element={<Home />} />
                     <Route path="about" element={<About />} />
+                    <Route path="signup" element={<SignUp />} />
                 </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
