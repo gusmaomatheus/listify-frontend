c7e6a8ab66e31d91e0ef3bcb03be356f266a9fb7
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-09-29 21:19:56 -0300

    feat: implement nested routes
    Implement nested routes using the LandingPage page to reuse the Header
    component that will be common on the Home and About pages.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,65 +1,61 @@
 import Text from "../../components/Text";
 import FeedbackCard from "../../components/FeedbackCard";
-import Header from "components/Header";
 
 export default function Home() {
     return (
-        <div className="flex flex-col">
-            <Header />
-            <main className="flex flex-col mt-[60px] space-y-[100px] mb-[90px]">
-                <section>
-                    <article>
-                        <div className="space-y-[8px]">
-                            <Text as="h2" size={32} weight={600} centralize>
-                                Join millions of people organizing their work and personal lives with{" "}
-                                <Text as="span" size={32} color="#318CE7" weight={600}>
-                                    listify
-                                </Text>
+        <main className="flex flex-col mt-[60px] space-y-[100px] mb-[90px]">
+            <section>
+                <article>
+                    <div className="space-y-[8px]">
+                        <Text as="h2" size={32} weight={600} centralize>
+                            Join millions of people organizing their work and personal lives with{" "}
+                            <Text as="span" size={32} color="#318CE7" weight={600}>
+                                listify
                             </Text>
-                            <div className="px-[40px]">
-                                <Text as="p" size={20} color="#818589" centralize>
-                                    Discover the ultimate solution to organize your life! With listify, you can simplify
-                                    your day and ensure nothing gets left behind. Want yo stay o top of all your
-                                    responsibilities, both at work and in your personal life? listify is perfect tool
-                                    for you! With an intuitive interface and smart features, you can easily create, edit
-                                    and manage your tasks. Boost your productivity and eliminate stress by taking full
-                                    controll of your lists. Try it now and turn your tasks into achievements!
-                                </Text>
-                            </div>
-                        </div>
-                    </article>
-                </section>
-                <section className="self-center flex flex-col w-[60%]">
-                    <article className="space-y-[40px]">
-                        <div className="space-y-[8px]">
-                            <Text as="h2" size={32} weight={600} centralize>
-                                Feedbacks
-                            </Text>
+                        </Text>
+                        <div className="px-[40px]">
                             <Text as="p" size={20} color="#818589" centralize>
-                                Bellow are testimonials from some of our customers about listify.
+                                Discover the ultimate solution to organize your life! With listify, you can simplify
+                                your day and ensure nothing gets left behind. Want yo stay o top of all your
+                                responsibilities, both at work and in your personal life? listify is perfect tool for
+                                you! With an intuitive interface and smart features, you can easily create, edit and
+                                manage your tasks. Boost your productivity and eliminate stress by taking full controll
+                                of your lists. Try it now and turn your tasks into achievements!
                             </Text>
                         </div>
-                        <div className="flex justify-center space-x-[40px]">
-                            <FeedbackCard
-                                name="James Parker"
-                                feedback="This app keeps me organized and on track every day. Super easy to use!"
-                            />
-                            <FeedbackCard
-                                name="Emily Johnson"
-                                feedback="Love how simple and effective this app is. My productivity has skyrocketed!"
-                            />
-                            <FeedbackCard
-                                name="Michael Davis"
-                                feedback="Best to-do list app I've found. Quick, clean, and keeps me focused!"
-                            />
-                            <FeedbackCard
-                                name="Sarah Collins"
-                                feedback="So intuitive! Managing my tasks has never been this effortless."
-                            />
-                        </div>
-                    </article>
-                </section>
-            </main>
-        </div>
+                    </div>
+                </article>
+            </section>
+            <section className="self-center flex flex-col w-[60%]">
+                <article className="space-y-[40px]">
+                    <div className="space-y-[8px]">
+                        <Text as="h2" size={32} weight={600} centralize>
+                            Feedbacks
+                        </Text>
+                        <Text as="p" size={20} color="#818589" centralize>
+                            Bellow are testimonials from some of our customers about listify.
+                        </Text>
+                    </div>
+                    <div className="flex justify-center space-x-[40px]">
+                        <FeedbackCard
+                            name="James Parker"
+                            feedback="This app keeps me organized and on track every day. Super easy to use!"
+                        />
+                        <FeedbackCard
+                            name="Emily Johnson"
+                            feedback="Love how simple and effective this app is. My productivity has skyrocketed!"
+                        />
+                        <FeedbackCard
+                            name="Michael Davis"
+                            feedback="Best to-do list app I've found. Quick, clean, and keeps me focused!"
+                        />
+                        <FeedbackCard
+                            name="Sarah Collins"
+                            feedback="So intuitive! Managing my tasks has never been this effortless."
+                        />
+                    </div>
+                </article>
+            </section>
+        </main>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import NotFound from "pages/NotFound";
+import About from "pages/About";
+import LandingPage from "pages/LandingPage";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route path="/" element={<LandingPage />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
